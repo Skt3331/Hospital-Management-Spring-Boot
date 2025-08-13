595648a9605ade30f6f7530da13952ed08c31f90
skt3331 <sauravtemgire@gmail.com>
2025-08-13 19:12:10 +0530

    native Query (SQL Query)

--- src/main/java/Repository/HospitalRepository.java
+++ src/main/java/Repository/HospitalRepository.java
@@ -10,6 +10,8 @@ public interface HospitalRepository extends JpaRepository<Hospital, Long> {
     List<Hospital> findByHospitalName(String name); // follow field name in entity
     List<Hospital> findByHospitalNameAndHospitalAddress(String hospitalName, String hospitalAddress);
      @Query("SELECT h from Hospital h where h.hospitalName=?1 and h.hospitalAddress=?2")
+     // If we want to pass native means sql Quert we want to use argument like
+   //  @Query("SELECT * from Hospital where hospitalName=?1 and hospitalAddress=?2",nativeQuery=true)
     List<Hospital> findByNL(String hospitalName, String hospitalAddress);
 
 }
