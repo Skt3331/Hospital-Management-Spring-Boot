702d3e00c172f2dc6a5653ee4d5d81f3795b63c9
skt3331 <sauravtemgire@gmail.com>
2025-08-13 19:02:01 +0530

    Added Custom HQL Query to get Data from Database TO Hibernate Jpa

--- src/main/java/Repository/HospitalRepository.java
+++ src/main/java/Repository/HospitalRepository.java
@@ -2,10 +2,14 @@ package Repository;
 
 import Entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
     List<Hospital> findByHospitalName(String name); // follow field name in entity
     List<Hospital> findByHospitalNameAndHospitalAddress(String hospitalName, String hospitalAddress);
+     @Query("SELECT h from Hospital h where h.hospitalName=?1 and h.hospitalAddress=?2")
+    List<Hospital> findByNL(String hospitalName, String hospitalAddress);
 
 }
